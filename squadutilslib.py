3645409c1b88baf689514830a60321ef773a2228
Katie Worton <katie.worton@linaro.org>
2023-04-17 20:41:22 +0100

    Add support for fetching TuxRun reproducers
    Add support for finding a TuxRun reproducer for a given group, project,
    device, suite and build name.
    
    Signed-off-by: Katie Worton <katie.worton@linaro.org>

---
+++ squadutilslib.py
@@ -0,0 +1,168 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# vim: set ts=4
+#
+# Copyright 2023-present Linaro Limited
+#
+# SPDX-License-Identifier: MIT
+
+
+import logging
+import os
+import re
+from pathlib import Path
+
+import requests
+from requests import HTTPError
+from squad_client.core.models import Build, Squad, TestRun
+from squad_client.shortcuts import download_tests
+from squad_client.utils import first, getid
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+class ReproducerNotFound(Exception):
+    """
+    Raised when no reproducer can be found.
+    """
+
+    def __init__(self, message="No reproducer found"):
+        super().__init__(message)
+
+
+def get_file(path, filename=None):
+    """
+    Download file if a URL is passed in, then return the filename of the
+    downloaded file. If an existing file path is passed in, return the path. If
+    a non-existent path is passed in, raise an exception.
+    """
+    logger.info(f"Getting file from {path}")
+    if re.search(r"https?://", path):
+        request = requests.get(path, allow_redirects=True)
+        request.raise_for_status()
+        if not filename:
+            filename = path.split("/")[-1]
+        else:
+            output_file = Path(filename)
+            output_file.parent.mkdir(exist_ok=True, parents=True)
+
+        with open(filename, "wb") as f:
+            f.write(request.content)
+        return filename
+    elif os.path.exists(path):
+        return path
+    else:
+        raise Exception(f"Path {path} not found")
+
+
+def find_first_good_testrun(
+    build_names, builds, suite_names, envs, project, output_filename="result.txt"
+):
+    """
+    Given a list of builds IDs to choose from in a project, find the first one
+    that has a match for the build name, suite names and environments
+    """
+
+    # Given a list of builds IDs, find one that contains the suites and that
+    # has a matching build name
+    for build in builds.values():
+        suites = []
+        # only pick builds that are finished
+        if not build.finished:
+            logger.info(f"Skipping {build.id} as build is not marked finished")
+            continue
+        logger.info(f"Checking build {build.id}")
+        # Create the list of suite IDs from the suite names
+        if suite_names:
+            for s in suite_names:
+                suites += project.suites(slug=s).values()
+
+        if os.path.exists(output_filename):
+            os.remove(output_filename)
+
+        # Use download_tests to gather filtered test results (build_name does not
+        # currently work as a filter)
+        download_tests(
+            project,
+            build,
+            envs,
+            suites,
+            "{test.test_run.metadata.build_name}/{test.test_run.id}",
+            output_filename,
+        )
+
+        # Look in the file that contains the downloaded tests and see if there is a
+        # match for one of the desired build_names
+        file_open = open(output_filename, "r")
+        file_lines = file_open.readlines()
+
+        for line in file_lines:
+            build_name, run_id = line.split("/")
+            for allowed_build_name in build_names:
+                re_match = re.match(f"^{allowed_build_name}$", build_name)
+                if re_match:
+                    # Return a TestRun with a matching build name, project, environment, suite
+                    # if it exists
+                    return TestRun(run_id)
+
+    # If no matching testrun found
+    return None
+
+
+def get_reproducer(
+    group, project, device_name, debug, build_names, suite_name, count, filename
+):
+    """
+    Given a group, project, device and accepted build names, return a
+    reproducer for a test run that meets these conditions.
+    """
+    if debug:
+        logger.setLevel(level=logging.DEBUG)
+
+    base_group = Squad().group(group)
+    if base_group is None:
+        logger.error(f"Get group failed. Group not found: {group}")
+        raise ReproducerNotFound
+
+    base_project = base_group.project(project)
+    if base_project is None:
+        logger.error(f"Get project failed. project not found: {project}")
+        raise ReproducerNotFound
+
+    logger.debug(f"build name {build_names}")
+
+    metadata = first(Squad().suitemetadata(suite=suite_name, kind="test"))
+
+    if metadata is None:
+        logger.error(f"There is no suite named: {suite_name}")
+        raise ReproducerNotFound
+
+    # == get a build that contains a run of the specified suite ==
+
+    # Get the latest N builds in the project so we don't pick something old
+    builds = base_project.builds(count=count, ordering="-id")
+    environment = base_project.environment(device_name)
+
+    logger.debug("Find build")
+    testrun = find_first_good_testrun(
+        build_names, builds, [suite_name], [environment], base_project
+    )
+
+    # Get the reproducer if a testrun is found
+    if testrun:
+        logger.info(
+            f"Found testrun {testrun} with build_name {testrun.metadata.build_name}, url: {testrun.url}"
+        )
+
+        # In theory there should only be one of those
+        logger.debug(f"Testrun id: {testrun.id}")
+
+        try:
+            tuxrun = get_file(f"{testrun.job_url}/reproducer", filename=filename)
+        except HTTPError:
+            logger.error(f"Reproducer not found at {testrun.job_url}!")
+            raise ReproducerNotFound
+        return tuxrun
+    else:
+        raise ReproducerNotFound
