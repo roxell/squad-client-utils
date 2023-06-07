d73f35fb304eadd94fbb17abdeaf7cf561237d63
Anders Roxell <anders.roxell@linaro.org>
2023-06-07 21:46:27 +0200

    fix black formatting issues
    Signed-off-by: Anders Roxell <anders.roxell@linaro.org>

--- find_latest_passing_test.py
+++ find_latest_passing_test.py
@@ -14,58 +14,69 @@ from squad_client.core.models import Squad, ALL, Build
 from squad_client.core.api import SquadApi
 from squad_client.utils import first
 
+
 def main(args):
     # Some configuration, might get parameterized later
-    SquadApi.configure(args.get('squadapi_url', None))
+    SquadApi.configure(args.get("squadapi_url", None))
     squad = Squad()
-    getid = lambda s: int(re.search('\d+', s).group())
-    group = squad.group(args.get('group', None))
-    project = group.project(args.get('project', None))
-    bad_suite = project.suite(args.get('suite', None))
-    bad_test = args.get('test', None)
-    bad_env = project.environment(args.get('arch', None))
+    getid = lambda s: int(re.search("\d+", s).group())
+    group = squad.group(args.get("group", None))
+    project = group.project(args.get("project", None))
+    bad_suite = project.suite(args.get("suite", None))
+    bad_test = args.get("test", None)
+    bad_env = project.environment(args.get("arch", None))
 
-    kernel_build = args.get('kernel_build', None)
+    kernel_build = args.get("kernel_build", None)
     if kernel_build:
         bad_build = project.build(kernel_build)
     else:
-        bad_build = first(project.builds(count=1, ordering='-id'))
+        bad_build = first(project.builds(count=1, ordering="-id"))
 
-    print('Looking at the next good build in %s/%s for build %s' % (group.slug, project.slug, bad_build.version), flush=True)
+    print(
+        "Looking at the next good build in %s/%s for build %s"
+        % (group.slug, project.slug, bad_build.version),
+        flush=True,
+    )
 
     tests = squad.tests(
         build__created_at__lt=bad_build.created_at,
         suite=bad_suite.id,
         environment=bad_env.id,
         metadata__name=bad_test,
-        ordering='-build_id',
+        ordering="-build_id",
         result=True,
-        count=1
+        count=1,
     )
 
     if len(tests):
         test = first(tests)
         build = Build(getid(test.build))
-        print('%s: https://qa-reports.linaro.org/%s/%s/build/%s' % (build.version, group.slug, project.slug, build.version))
+        print(
+            "%s: https://qa-reports.linaro.org/%s/%s/build/%s"
+            % (build.version, group.slug, project.slug, build.version)
+        )
     else:
-        print('No good build')
+        print("No good build")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--kernel_build",
-                        help="tag or sha, e.g., next-20201204. Defaults to latest build")
-    parser.add_argument("--arch", required=True,
-                        help="architecture, e.g., arm64")
-    parser.add_argument("--group", required=True,
-                        help="Group name e.g., lkft")
-    parser.add_argument("--project", required=True,
-                        help="Project name e.g., linux-next-master")
-    parser.add_argument("--suite", required=True,
-                        help="Suite name e.g., kunit")
-    parser.add_argument("--test", required=True,
-                        help="test name")
-    parser.add_argument("--squadapi_url", default='https://qa-reports.linaro.org',
-                        help="url to SQUAD server")
+    parser.add_argument(
+        "--kernel_build",
+        help="tag or sha, e.g., next-20201204. Defaults to latest build",
+    )
+    parser.add_argument("--arch", required=True, help="architecture, e.g., arm64")
+    parser.add_argument("--group", required=True, help="Group name e.g., lkft")
+    parser.add_argument(
+        "--project", required=True, help="Project name e.g., linux-next-master"
+    )
+    parser.add_argument("--suite", required=True, help="Suite name e.g., kunit")
+    parser.add_argument("--test", required=True, help="test name")
+    parser.add_argument(
+        "--squadapi_url",
+        default="https://qa-reports.linaro.org",
+        help="url to SQUAD server",
+    )
     args = vars(parser.parse_args())
     if args:
         main(args)
--- find_stable_tests.py
+++ find_stable_tests.py
@@ -17,18 +17,18 @@ do_color = False
 
 def _color(string, color):
     colors = {
-        'red': '\033[31m',
-        'green': '\033[42m',
-        'yellow': '\033[33m',
+        "red": "\033[31m",
+        "green": "\033[42m",
+        "yellow": "\033[33m",
     }
 
-    end = '\033[0m'
+    end = "\033[0m"
     return colors[color] + string + end
 
 
-red = lambda s: _color(s, 'red') if do_color else s
-yellow = lambda s: _color(s, 'yellow') if do_color else s
-green = lambda s: _color(s, 'green') if do_color else s
+red = lambda s: _color(s, "red") if do_color else s
+yellow = lambda s: _color(s, "yellow") if do_color else s
+green = lambda s: _color(s, "green") if do_color else s
 
 
 def join(array, separator=","):
@@ -37,18 +37,18 @@ def join(array, separator=","):
 
 def stableness(results, target="pass", pad=10):
     """
-        A test is stable when all its results are "pass".
-        Any test that doesn't fit that rule will get a 
-        number that represents the number of "pass" divided
-        by the total number of results.
+    A test is stable when all its results are "pass".
+    Any test that doesn't fit that rule will get a
+    number that represents the number of "pass" divided
+    by the total number of results.
     """
 
     if len(results) == 0:
-        return -1, red('N/A'.center(pad))
+        return -1, red("N/A".center(pad))
 
     pass_count = results.count(target)
     n = pass_count / len(results)
-    out = str(round(n * 100)) + '%'
+    out = str(round(n * 100)) + "%"
 
     color = str
     if do_color:
@@ -62,20 +62,20 @@ def stableness(results, target="pass", pad=10):
 
 def find_stable_tests(tests, envs={}, suites={}):
     """
-        Print a list of stable tests
+    Print a list of stable tests
 
-        - if environments are given
+    - if environments are given
 
-                       | envA | envb |
-          suiteA       |  2/2 | 1/2  |
-          - testA      |  Y   |  90% |
-          - testB      |  Y   |  80% |
-          suiteB/testA |  50% |  Y   |
+                   | envA | envb |
+      suiteA       |  2/2 | 1/2  |
+      - testA      |  Y   |  90% |
+      - testB      |  Y   |  80% |
+      suiteB/testA |  50% |  Y   |
 
     """
 
     if len(tests) == 0:
-        print('*** No tests available ***')
+        print("*** No tests available ***")
         return
 
     envs_slugs = sorted([env.slug for env in envs.values()])
@@ -83,7 +83,9 @@ def find_stable_tests(tests, envs={}, suites={}):
     tests_dict = {}
 
     # Longest test name
-    longest_test_name = max([len(parse_test_name(name)[1]) for name in tests_names]) + len('- ')
+    longest_test_name = max(
+        [len(parse_test_name(name)[1]) for name in tests_names]
+    ) + len("- ")
 
     # Saves a summary by suite
     suites_stableness = defaultdict(list)
@@ -98,40 +100,40 @@ def find_stable_tests(tests, envs={}, suites={}):
             env = envs[getid(test.environment)]
             tests_dict[test.name][env.slug].append(test.status)
 
-        print(' ' * (3 + longest_test_name), end='')
-        envs_header = '|'.join([slug.center(longest_env_slug) for slug in envs_slugs])
-        print(f'|{envs_header}|')
-        
+        print(" " * (3 + longest_test_name), end="")
+        envs_header = "|".join([slug.center(longest_env_slug) for slug in envs_slugs])
+        print(f"|{envs_header}|")
+
         prev_suite_slug = None
         for test_name in tests_names:
-
             suite_slug, test = parse_test_name(test_name)
             if suite_slug != prev_suite_slug:
                 prev_suite_slug = suite_slug
-                print(f'\n\033[1m{prev_suite_slug}\033[0m')
+                print(f"\n\033[1m{prev_suite_slug}\033[0m")
 
-            print(f'- {test.ljust(longest_test_name)} ', end='')
+            print(f"- {test.ljust(longest_test_name)} ", end="")
             for env_slug in envs_slugs:
-                n, out = stableness(tests_dict[test_name][env_slug], pad=longest_env_slug)
+                n, out = stableness(
+                    tests_dict[test_name][env_slug], pad=longest_env_slug
+                )
                 suites_stableness[suite_slug].append(n)
-                print(f'|{out}', end='')
-            print('|')
+                print(f"|{out}", end="")
+            print("|")
     else:
         tests_dict = defaultdict(list)
         for test in tests:
             tests_dict[test.name].append(test.status)
-        
+
         prev_suite_slug = None
         for test_name in tests_names:
-
             suite_slug, test = parse_test_name(test_name)
             if suite_slug != prev_suite_slug:
                 prev_suite_slug = suite_slug
-                print(f'\n\033[1m{prev_suite_slug}\033[0m')
+                print(f"\n\033[1m{prev_suite_slug}\033[0m")
 
             n, out = stableness(tests_dict[test_name])
             suites_stableness[suite_slug].append(n)
-            print(f'- {test.ljust(longest_test_name)} {out}')
+            print(f"- {test.ljust(longest_test_name)} {out}")
 
     longest_suite_slug = max([len(slug) for slug in suites_stableness.keys()])
 
@@ -139,20 +141,19 @@ def find_stable_tests(tests, envs={}, suites={}):
     print("*** Suite summary ***")
     for suite_slug in suites_stableness.keys():
         n, out = stableness(suites_stableness[suite_slug], target=1)
-        print(f'\033[1m{suite_slug.ljust(longest_suite_slug)}\033[0m: {out}')
+        print(f"\033[1m{suite_slug.ljust(longest_suite_slug)}\033[0m: {out}")
 
 
 def main(args):
-
     global do_color
 
     do_color = args.color
 
     SquadApi.configure(args.squadapi_url)
     squad = Squad()
-    print(f'I: Fetching group {args.group}')
+    print(f"I: Fetching group {args.group}")
     group = squad.group(args.group)
-    print(f'I: Fetching project {args.project}')
+    print(f"I: Fetching project {args.project}")
     project = group.project(args.project)
 
     build_filters = {}
@@ -166,40 +167,40 @@ def main(args):
         test_filters["metadata__name__in"] = join(args.tests)
 
     if args.suites and len(args.suites):
-        print(f'I: Fetching {args.group}/{args.project} suites ({args.suites})')
+        print(f"I: Fetching {args.group}/{args.project} suites ({args.suites})")
         suites = project.suites(slug__in=join(args.suites))
         test_filters["suite__id__in"] = join([str(_id) for _id in suites.keys()])
     else:
-        print(f'I: Fetching {args.group}/{args.project} suites')
+        print(f"I: Fetching {args.group}/{args.project} suites")
         suites = project.suites()
 
     if args.no_arch:
         envs = {}
     elif args.archs and len(args.archs):
-        print(f'I: Fetching {args.group}/{args.project} environments ({args.archs})')
+        print(f"I: Fetching {args.group}/{args.project} environments ({args.archs})")
         envs = project.environments(slug__in=join(args.archs))
         test_filters["environment__id__in"] = join([str(_id) for _id in envs.keys()])
     else:
-        print(f'I: Fetching {args.group}/{args.project} environments')
+        print(f"I: Fetching {args.group}/{args.project} environments")
         envs = project.environments()
 
     tests = []
-    print(f'I: Fetching {args.n} builds ({build_filters}):', flush=True)
+    print(f"I: Fetching {args.n} builds ({build_filters}):", flush=True)
     for build in project.builds(**build_filters).values():
-        print(f'D: Fetching build {build.version} tests ({test_filters})', flush=True)
+        print(f"D: Fetching build {build.version} tests ({test_filters})", flush=True)
         num_tests = 0
         for test in build.tests(**test_filters).values():
-            if test.name.startswith('linux-log-parser'):
+            if test.name.startswith("linux-log-parser"):
                 continue
             tests.append(test)
             if num_tests % 1000 == 0:
-                print('.', end='', flush=True)
+                print(".", end="", flush=True)
             num_tests += 1
 
         if num_tests:
             print()
-        
-    print('I: Finding stable tests')
+
+    print("I: Finding stable tests")
     find_stable_tests(
         tests,
         envs=envs,
@@ -207,28 +208,55 @@ def main(args):
     )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--group", default="lkft",
-                        help="Group name e.g., lkft")
-    parser.add_argument("--project", required=True,
-                        help="Project name e.g., linux-next-master")
-    parser.add_argument("--builds", nargs="*",
-                        help="a list of builds (tag/sha) or the number of builds desired, e.g., next-20201204 or -n 10 . If none given, the 10 latest builds are chosen")
-    parser.add_argument("-n", default=10, type=int,
-                        help="Number of builds to lookup, defaults to 10. If none given, the 10 latest builds are chosen")
-    parser.add_argument("--archs", nargs="*",
-                        help="architectures to filter e.g. arm6 x86. If none given, show across all environments")
-    parser.add_argument("--no-arch", action="store_true", default=False,
-                        help="Ignore checking for archs")
-    parser.add_argument("--suites", nargs="*",
-                        help="Suite names to filter e.g., kunit ltp-math. If none given, show ")
-    parser.add_argument("--tests", nargs="*",
-                        help="Test names to filter e.g. my-test-1 my-test-2 Show data for specific tests only")
-    parser.add_argument("--squadapi_url", default='https://qa-reports.linaro.org',
-                        help="url to SQUAD server")
-    parser.add_argument("--color", action="store_true", default=False,
-                        help="Color output with green (100%), yellow (> 80%) or red")
+    parser.add_argument("--group", default="lkft", help="Group name e.g., lkft")
+    parser.add_argument(
+        "--project", required=True, help="Project name e.g., linux-next-master"
+    )
+    parser.add_argument(
+        "--builds",
+        nargs="*",
+        help="a list of builds (tag/sha) or the number of builds desired, e.g., next-20201204 or -n 10 . If none given, the 10 latest builds are chosen",
+    )
+    parser.add_argument(
+        "-n",
+        default=10,
+        type=int,
+        help="Number of builds to lookup, defaults to 10. If none given, the 10 latest builds are chosen",
+    )
+    parser.add_argument(
+        "--archs",
+        nargs="*",
+        help="architectures to filter e.g. arm6 x86. If none given, show across all environments",
+    )
+    parser.add_argument(
+        "--no-arch",
+        action="store_true",
+        default=False,
+        help="Ignore checking for archs",
+    )
+    parser.add_argument(
+        "--suites",
+        nargs="*",
+        help="Suite names to filter e.g., kunit ltp-math. If none given, show ",
+    )
+    parser.add_argument(
+        "--tests",
+        nargs="*",
+        help="Test names to filter e.g. my-test-1 my-test-2 Show data for specific tests only",
+    )
+    parser.add_argument(
+        "--squadapi_url",
+        default="https://qa-reports.linaro.org",
+        help="url to SQUAD server",
+    )
+    parser.add_argument(
+        "--color",
+        action="store_true",
+        default=False,
+        help="Color output with green (100%), yellow (> 80%) or red",
+    )
 
     args = parser.parse_args()
     main(args)
