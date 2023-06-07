498d9d67e1b2bb958fb46bc290695058edac1625
Anders Roxell <anders.roxell@linaro.org>
2023-06-07 21:46:27 +0200

    fix black formatting issues
    Signed-off-by: Anders Roxell <anders.roxell@linaro.org>

--- get_metrics.py
+++ get_metrics.py
@@ -1,16 +1,25 @@
 from squad_client.core.models import Squad, ALL
 
 
-group_slug = 'lkft'
-project_slug = 'linux-mainline-master'
+group_slug = "lkft"
+project_slug = "linux-mainline-master"
 
 
 group = Squad().group(group_slug)
 project = group.project(project_slug)
-builds = project.builds(count=10, created_at__lt='2020-09-30T20:40:50.341386Z')
+builds = project.builds(count=10, created_at__lt="2020-09-30T20:40:50.341386Z")
 
 for build in builds.values():
-    print('Getting metrics for build %s' % build.version)
-    metrics = Squad().metrics(fields='id,short_name,result,test_run,suite', ordering='name', count=ALL, test_run__build=build.id).values()
+    print("Getting metrics for build %s" % build.version)
+    metrics = (
+        Squad()
+        .metrics(
+            fields="id,short_name,result,test_run,suite",
+            ordering="name",
+            count=ALL,
+            test_run__build=build.id,
+        )
+        .values()
+    )
     for metric in metrics:
-        print('\t%s: %.2f' % (metric.short_name, metric.result))
+        print("\t%s: %.2f" % (metric.short_name, metric.result))
--- get_suite_runs.py
+++ get_suite_runs.py
@@ -12,21 +12,26 @@ from collections import defaultdict
 from squad_client.core.models import Squad, ALL
 from squad_client.core.api import SquadApi
 
+
 def main(args):
     # Some configuration, might get parameterized later
-    group_slug = args.get('group', None)
-    suite_slug = args.get('suite', None)
-    SquadApi.configure(args.get('squadapi_url', None))
-    number_of_builds = args.get('number', None)
+    group_slug = args.get("group", None)
+    suite_slug = args.get("suite", None)
+    SquadApi.configure(args.get("squadapi_url", None))
+    number_of_builds = args.get("number", None)
     squad = Squad()
-    getid = lambda s: int(re.search('\d+', s).group())
+    getid = lambda s: int(re.search("\d+", s).group())
 
     # First we need to know which projects from the selected group
     # contain the specified suite.
-    print('Fetching projects that contain "%s" suites for "%s" group' % (suite_slug, group_slug), flush=True)
+    print(
+        'Fetching projects that contain "%s" suites for "%s" group'
+        % (suite_slug, group_slug),
+        flush=True,
+    )
     suites = squad.suites(slug=suite_slug, project__group__slug=group_slug)
     projects_ids = list(set([str(getid(suite.project)) for suite in suites.values()]))
-    projects = squad.projects(id__in=','.join(projects_ids), ordering='slug').values()
+    projects = squad.projects(id__in=",".join(projects_ids), ordering="slug").values()
 
     # Env/arch cache
     environments = set()
@@ -50,51 +55,53 @@ def main(args):
 
     if number_of_builds == "0":
         for project in projects:
-            print('- %s' % project.slug, flush=True)
+            print("- %s" % project.slug, flush=True)
         return
 
     for project in projects:
-        print('- %s: fetching %s builds' % (project.slug, number_of_builds), flush=True)
+        print("- %s: fetching %s builds" % (project.slug, number_of_builds), flush=True)
 
         environments = project.environments(count=ALL)
 
-        for build in project.builds(count=int(number_of_builds), ordering='-id').values():
-            print('  - %s: fetching tests' % build.version, flush=True)
-            results = {'summary': defaultdict(dict)}
+        for build in project.builds(
+            count=int(number_of_builds), ordering="-id"
+        ).values():
+            print("  - %s: fetching tests" % build.version, flush=True)
+            results = {"summary": defaultdict(dict)}
 
             for test in build.tests(suite__slug=suite_slug).values():
-
                 env = environments[getid(test.environment)].slug
 
-                if test.status not in results['summary'][env]:
-                    results['summary'][env][test.status] = 0
-                results['summary'][env][test.status] += 1
+                if test.status not in results["summary"][env]:
+                    results["summary"][env][test.status] = 0
+                results["summary"][env][test.status] += 1
 
                 if env not in results:
                     results[env] = []
                 results[env].append((test.name, test.status))
 
-            if len(results['summary']):
-                print('    - summary:', flush=True)
-                summary = results.pop('summary')
+            if len(results["summary"]):
+                print("    - summary:", flush=True)
+                summary = results.pop("summary")
                 for env in sorted(summary.keys()):
-                    print('      - %s: %s' % (env, summary[env]), flush=True)
+                    print("      - %s: %s" % (env, summary[env]), flush=True)
 
                 for env in sorted(results.keys()):
-                    print('    - %s:' % env, flush=True)
+                    print("    - %s:" % env, flush=True)
                     for test in sorted(results[env], key=lambda d: d[0]):
-                        print('      - %s: %s' % (test[0], test[1]), flush=True)
+                        print("      - %s: %s" % (test[0], test[1]), flush=True)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--group", required=True,
-                        help="Group name e.g., lkft")
-    parser.add_argument("--suite", required=True,
-                        help="Suite name e.g., kunit")
-    parser.add_argument("--squadapi_url", default='https://qa-reports.linaro.org',
-                        help="url to SQUAD server")
-    parser.add_argument("--number", default='0',
-                        help="number of builds, default 0")
+    parser.add_argument("--group", required=True, help="Group name e.g., lkft")
+    parser.add_argument("--suite", required=True, help="Suite name e.g., kunit")
+    parser.add_argument(
+        "--squadapi_url",
+        default="https://qa-reports.linaro.org",
+        help="url to SQUAD server",
+    )
+    parser.add_argument("--number", default="0", help="number of builds, default 0")
     args = vars(parser.parse_args())
     if args:
         main(args)
